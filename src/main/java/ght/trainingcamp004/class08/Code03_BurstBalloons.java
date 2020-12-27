a9c535dec8d212b9774285d67f3d742d751c93b3
gui <925630328@qq.com>
2020-12-27 19:05:04 +0800

    learning

--- src/main/java/ght/trainingcamp004/class08/Code03_BurstBalloons.java
+++ src/main/java/ght/trainingcamp004/class08/Code03_BurstBalloons.java
@@ -45,35 +45,33 @@ public class Code03_BurstBalloons {
 		if (arr.length == 1) {
 			return arr[0];
 		}
-		int N = arr.length;
-		int[] help = new int[N + 2];
+		int n = arr.length;
+		int[] help = new int[n + 2];
 		help[0] = 1;
-		help[N + 1] = 1;
-		for (int i = 0; i < N; i++) {
-			help[i + 1] = arr[i];
-		}
-		int[][] dp = new int[N + 2][N + 2];
-		for (int i = 1; i <= N; i++) {
+		help[n + 1] = 1;
+		System.arraycopy(arr, 0, help, 1, n);
+		int[][] dp = new int[n + 2][n + 2];
+		for (int i = 1; i <= n; i++) {
 			dp[i][i] = help[i - 1] * help[i] * help[i + 1];
 			System.out.println(dp[i][i]);
 		}
-		for (int L = N; L >= 1; L--) {
-			for (int R = L + 1; R <= N; R++) {
+		for (int l = n; l >= 1; l--) {
+			for (int r = l + 1; r <= n; r++) {
 				// 求解dp[L][R]，表示help[L..R]上打爆所有气球的最大分数
 				// 最后打爆help[L]的方案
-				int finalL = help[L - 1] * help[L] * help[R + 1] + dp[L + 1][R];
+				int finalL = help[l - 1] * help[l] * help[r + 1] + dp[l + 1][r];
 				// 最后打爆help[R]的方案
-				int finalR = help[L - 1] * help[R] * help[R + 1] + dp[L][R - 1];
+				int finalR = help[l - 1] * help[r] * help[r + 1] + dp[l][r - 1];
 				// 最后打爆help[L]的方案，和最后打爆help[R]的方案，先比较一下
-				dp[L][R] = Math.max(finalL, finalR);
+				dp[l][r] = Math.max(finalL, finalR);
 				// 尝试中间位置的气球最后被打爆的每一种方案
-				for (int i = L + 1; i < R; i++) {
-					dp[L][R] = Math.max(dp[L][R], help[L - 1] * help[i]
-							* help[R + 1] + dp[L][i - 1] + dp[i + 1][R]);
+				for (int i = l + 1; i < r; i++) {
+					dp[l][r] = Math.max(dp[l][r], help[l - 1] * help[i]
+							* help[r + 1] + dp[l][i - 1] + dp[i + 1][r]);
 				}
 			}
 		}
-		return dp[1][N];
+		return dp[1][n];
 	}
 
 	public static void main(String[] args) {
